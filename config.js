7f4fbc682f7a51151672fd7227bc85a7455ee6db
abinashTech96 <abinashmoharana.github@gmail.com>
2026-07-04 02:37:50 +0530

    Insert Gemini API Key in config.js
    Added Gemini API Key for local testing.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const CONFIG = {
     // ⚠️ Insert your Gemini API Key here for local testing
-    GEMINI_API_KEY: '',
+    GEMINI_API_KEY: 'AQ.Ab8RN6KjB2Zh-q2WRfUWEGV1exWaGNUcPzhjUpqA_iV7KJ64SQ',
     
     // When you build your Java/Python backend, you will simply change this URL
     // e.g., AI_ENDPOINT: 'http://localhost:8080/api/architect'
